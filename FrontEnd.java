d88de2841f615accf2c6f693cb01f5fc7761f2fb
hstrohbusch <71783165+hstrohbusch@users.noreply.github.com>
2020-10-18 14:28:46 -0500

    Add files via upload

--- FrontEnd.java
+++ FrontEnd.java
@@ -22,75 +22,130 @@ public class FrontEnd {
 		Movie tempMovie = new Movie("", "", "", -1, new String[]{"",""}, 1, 1.0);
 
 		System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~  Welcome to the Movie Database  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-		System.out.println("Please make a selection by entering the number of one of the options below and pressing enter");
-		System.out.println("---------------------------------------------------------------------------------------------");
 
-		System.out.println("1.) Search for a Movie");
-		System.out.println("2.) Exit");
-		System.out.println("---------------------------------------------------------------------------------------------");
-
-		cur = intScan.nextInt();
-
-		if(cur == 2) cur = 8;
-
-		while(cur!=8) {
+		while(cur!=9) {
 
 			if(stay != true) {
-				System.out.println("---------------------------------------------------------------------------------------------");
-				System.out.println("Please enter the exact name of the movie that you "
-						+ "would like to learn about and press <Enter>:");
-				System.out.println("---------------------------------------------------------------------------------------------");
-				temp = stringScan.nextLine();
-				Movie dummy = new Movie(temp, "", "", -1, new String[]{"",""}, 1, 1.0);
-				tempMovie = findMovie(dummy, movies);
 
-			}
-
-			if(tempMovie == null) {	
+				System.out.println("Please make a selection by entering the number of one of the options below and pressing enter");
 				System.out.println("---------------------------------------------------------------------------------------------");
-				System.out.println("A movie with the title "+temp+" was not found:");
-				System.out.println("---------------------------------------------------------------------------------------------");}
-			else {
-				choices(temp);
+
+				System.out.println("1.) Search for a Movie");
+				System.out.println("2.) Add a new Movie to the Movie Database");
+				System.out.println("3.) Exit");
+				System.out.println("---------------------------------------------------------------------------------------------");
+
 				cur = intScan.nextInt();
+				
+				if(cur<1||cur>3)  { System.out.println("Invalid input"); }
 
-				System.out.println("---------------------------------------------------------------------------------------------");
-				if(cur == 1) {System.out.println("~~~ Title: " +tempMovie.getTitle()+" ~~~"); }
-				else if(cur == 2) {System.out.println("~~~ Genre: " +tempMovie.getGenre()+" ~~~"); }
-				else if(cur == 3) {System.out.println("~~~ Release Date: "+tempMovie.getReleaseDate()+" ~~~"); }
-				else if(cur == 4) {System.out.println("~~~ Actors: "+tempMovie.getActors()+" ~~~"); }
-				else if(cur == 5) {System.out.println("~~~ Run Time: "+tempMovie.getRunTime()+" ~~~"); }
-				else if(cur == 6) {System.out.println("~~~ Director: " +tempMovie.getDirector()+" ~~~"); }
-				else if(cur == 7) {System.out.println("~~~ Rating: " +tempMovie.getRatings()+" ~~~"); }		
-				else if(cur != 8) {System.out.println("Incorrect input");}
+				if(cur == 3) cur = 9;
 
-				System.out.println("---------------------------------------------------------------------------------------------");
+				if(cur ==1) { //finding a movie
+					System.out.println("---------------------------------------------------------------------------------------------");
+					System.out.println("Please enter the exact name of the movie that you "
+							+ "would like to learn about and press <Enter>:");
+					System.out.println("---------------------------------------------------------------------------------------------");
+					temp = stringScan.nextLine();
+					Movie dummy = new Movie(temp, "", "", -1, new String[]{"",""}, 1, 1.0);
+					tempMovie = findMovie(dummy, movies);
+				}
 
 			}
 
-			if(tempMovie != null) {
-				System.out.println("Would you like to learn more about this movie?\nEnter Y if yes and N if no");
-				System.out.println("---------------------------------------------------------------------------------------------");
-				temp = stringScan.nextLine();
-				if(temp.equalsIgnoreCase("y")) {stay = true;}
-				else {
+			if(cur ==1) { //looking at the found movie
+				if(tempMovie == null) {	
 					System.out.println("---------------------------------------------------------------------------------------------");
+					System.out.println("A movie with the title "+temp+" was not found:");
+					System.out.println("---------------------------------------------------------------------------------------------");}
+				else {
+					choices(temp);
+					cur = intScan.nextInt();
+
+					System.out.println("---------------------------------------------------------------------------------------------");
+					if(cur == 1) {System.out.println("~~~ Title: " +tempMovie.getTitle()+" ~~~"); }
+					else if(cur == 2) {System.out.println("~~~ Director: " +tempMovie.getDirector()+" ~~~"); }
+					else if(cur == 3) {System.out.println("~~~ Genre: " +tempMovie.getGenre()+" ~~~"); }
+					else if(cur == 4) {System.out.println("~~~ Run Time: "+tempMovie.getRunTime()+" ~~~"); }
+					else if(cur == 5) {System.out.println("~~~ Actors: "+tempMovie.getActors()+" ~~~"); }
+					else if(cur == 6) {System.out.println("~~~ Release Date: "+tempMovie.getReleaseDate()+" ~~~"); }
+					else if(cur == 7) {System.out.println("~~~ Rating: " +tempMovie.getRatings()+" ~~~"); }	
+					else if(cur == 8) {System.out.println(tempMovie.toString()); }	
+					else if(cur != 9) {System.out.println("Incorrect input");}
+					
+					cur = 1;
+
+					System.out.println("---------------------------------------------------------------------------------------------");
+
+				}
+
+				if(tempMovie != null) {
+					System.out.println("Would you like to learn more about this movie?\nEnter Y if yes and N if no");
+					System.out.println("---------------------------------------------------------------------------------------------");
+					temp = stringScan.nextLine();
+					if(temp.equalsIgnoreCase("y")) {stay = true;}
+					else if(temp.equalsIgnoreCase("n")){
+						System.out.println("---------------------------------------------------------------------------------------------");
+						System.out.println("Would you like to continue seaching the Movie Database?\nEnter Y if yes and N if no");
+						System.out.println("---------------------------------------------------------------------------------------------");
+						temp = stringScan.nextLine();
+						if(temp.equalsIgnoreCase("Y")) {stay = false;}
+						else if(temp.equalsIgnoreCase("N")) { cur = 9;}
+						else {System.out.println("Incorrect input: continuing search");}
+					}
+					else {System.out.println("Incorrect input: continuing search"); }
+				}
+				else {
 					System.out.println("Would you like to continue seaching the Movie Database?\nEnter Y if yes and N if no");
 					System.out.println("---------------------------------------------------------------------------------------------");
 					temp = stringScan.nextLine();
 					if(temp.equalsIgnoreCase("Y")) {stay = false;}
-					else if(temp.equalsIgnoreCase("N")) { cur = 8;}
+					else if(temp.equalsIgnoreCase("N")) { cur = 9;}
 					else {System.out.println("Incorrect input: continuing search");}
 				}
 			}
-			else {
+			
+			if(cur ==2) {//adding a new movie to the database
+				System.out.println("---------------------------------------------------------------------------------------------");
+				System.out.print("Please enter the Title of the movie that you want to add: ");
+				String title = stringScan.nextLine();
+				System.out.print("Please enter the Director of the movie that you want to add: ");
+				String director = stringScan.nextLine();
+				System.out.print("Please enter the Genre of the movie that you want to add: ");
+				String genre = stringScan.nextLine();
+				System.out.print("Please enter the Run Time of the movie that you want to add as a whole number: ");
+				int runTime = Integer.valueOf(stringScan.nextLine());
+				System.out.print("Please enter the Actors of the movie that you want to add seperated by commas: ");
+				String actorsAsString = stringScan.nextLine(); //needs to be an array
+				String[] actors = actorsAsString.split(", ");
+				System.out.print("Please enter the Release Date of the movie that you want to add: ");
+				int release = Integer.valueOf(stringScan.nextLine());
+				System.out.print("Please enter the Rating of the movie that you want to add as a number with a decimal point value: ");
+				Double rating = Double.valueOf(stringScan.nextLine());
+								
+				Movie added = new Movie(title, director, genre, runTime, actors, release, rating);
+				
+				try {
+					movies.insert(added);
+				} catch(IllegalArgumentException e1) {
+					System.out.println("That movie is already in the database");
+				} catch(Exception e) {
+					System.out.println("An unexpected error has occured when attempting to add the new movie, please try again.");
+				}
+				System.out.println("---------------------------------------------------------------------------------------------");
+				System.out.println("The movie that you just added:");
+				System.out.println(added);
+				
+				System.out.println("---------------------------------------------------------------------------------------------");
 				System.out.println("Would you like to continue seaching the Movie Database?\nEnter Y if yes and N if no");
 				System.out.println("---------------------------------------------------------------------------------------------");
 				temp = stringScan.nextLine();
 				if(temp.equalsIgnoreCase("Y")) {stay = false;}
-				else if(temp.equalsIgnoreCase("N")) { cur = 8;}
+				else if(temp.equalsIgnoreCase("N")) { cur = 9;}
 				else {System.out.println("Incorrect input: continuing search");}
+				
 			}
+			
 
 		}
 
@@ -124,13 +179,14 @@ public class FrontEnd {
 		System.out.println("---------------------------------------------------------------------------------------------");
 		System.out.println("What information about "+s+ " would you like to know?");
 		System.out.println("1.) Title");
-		System.out.println("2.) Genre");
-		System.out.println("3.) Release Date");
-		System.out.println("4.) Actors");
-		System.out.println("5.) Run Time");
-		System.out.println("6.) Director");
+		System.out.println("2.) Director");
+		System.out.println("3.) Genre");
+		System.out.println("4.) Run Time");
+		System.out.println("5.) Actors");
+		System.out.println("6.) Release Date");
 		System.out.println("7.) Rating");
-		System.out.println("8.) I am done with the program");
+		System.out.println("8.) All of the above");
+		System.out.println("9.) I am done with the program");
 		System.out.println("Please enter your choice as a number and press <Enter>");
 		System.out.println("---------------------------------------------------------------------------------------------");
 	}
